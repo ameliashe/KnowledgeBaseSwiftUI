994cfda05e154e0e7e3d573c32811e6c10181a14
ameliashe <ameliashe@icloud.com>
2025-04-23 05:01:18 +0400

    Initial commit

---
+++ KnowledgeBaseSwiftUI/KnowledgeBaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeBaseSwiftUIApp.swift
+//  KnowledgeBaseSwiftUI
+//
+//  Created by Amelia Romanova on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct KnowledgeBaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
